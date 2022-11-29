bea23fb11082357d29d111036cb60a6675ca83e2
sjiyeng <81696291+sjiyeng@users.noreply.github.com>
2022-11-28 23:17:32 -0500

    Yes the compressor is a servo don't as questions

--- test_board1.ino
+++ test_board1.ino
@@ -1,7 +1,7 @@
+#include <Servo.h>
 const int switch1 = 5;
 const int switch2 = 4;
 const int pressuresensor = A0;
-const int compressor = 6;
 const int voltagepin = A1;
 const int enablepin = 11;
 const int solenoid1F = 7;
@@ -15,20 +15,20 @@ int analogvoltagevalue = 0;
 int pressure = 0;
 int voltage = 0;
 int enablestate = 0;
+Servo compressor;
 
 void setup()
 {
   pinMode(switch1, INPUT);
   pinMode(switch2, INPUT);
   pinMode(voltagepin, INPUT);
-  pinMode(pressuresensor, INPUT);
-  pinMode(compressor, OUTPUT);
-  pinMode(solenoid1F, OUTPUT);
+  pinMode(pressuresensor, INPUT);  pinMode(solenoid1F, OUTPUT);
   pinMode(solenoid1R, OUTPUT);
   pinMode(solenoid2F, OUTPUT);
   pinMode(solenoid2R, OUTPUT);
   pinMode(enablepin, INPUT);
   Serial.begin(9600);
+  compressor.attach(6);
 }
 
 void loop()
@@ -36,16 +36,16 @@ void loop()
   //read switches
   switch1state = digitalRead(switch1);
   switch2state = digitalRead(switch2);
-    
-  
+
+
   // read and calculate pressure
   analogpressurevalue = analogRead(pressuresensor);
   pressure = analogpressurevalue * .1953125;
-  
+
   //read and calculate voltage
   analogvoltagevalue = analogRead(voltagepin);
   voltage = analogvoltagevalue * .025;
-  
+
   //set enable state
   if(digitalRead(enablepin) == 1 && voltage > 8){
     enablestate = 1;
@@ -53,15 +53,15 @@ void loop()
   else{
     enablestate = 0;
   }
-  
+
   //set compressor state
   if( pressure < 115 && enablestate == 1){
-    digitalWrite(compressor, HIGH);
+    compressor.writeMicroseconds(2000);
   }
   else{
-    digitalWrite(compressor, LOW);
-  }  
-  
+    compressor.writeMicroseconds(1500);
+  }
+
   //set solenoid 1
   if( switch1state == 1 && enablestate == 1){
     digitalWrite(solenoid1F, HIGH);
@@ -71,7 +71,7 @@ void loop()
     digitalWrite(solenoid1F, LOW);
     digitalWrite(solenoid1R, HIGH);
   }
-  
+
   //set solenoid 2
   if( switch2state == 1 && enablestate == 1){
     digitalWrite(solenoid2F, HIGH);
